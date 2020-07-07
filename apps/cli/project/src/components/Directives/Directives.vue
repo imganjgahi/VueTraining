8bda50e015f77fbd6f1d053e8b02add9029ac9c8
Mehran <imganjgahi@gmail.com>
2020-07-07 10:55:34 +0430

    multiple modifiers

--- apps/cli/project/src/components/Directives/Directives.vue
+++ apps/cli/project/src/components/Directives/Directives.vue
@@ -1,25 +1,66 @@
 <template>
-    <div class="directive">
-        <h1>Build-in Directives</h1>
-        <p v-text="'Some Text'"></p>
-        <p v-html="'<b><i> Some Text  </i></b>'"></p>
-        <hr>
-        <h1>Custom Directive</h1>
-        <p v-highlight="'red'"> Color Changed</p>
-        <p v-highlight:background="'lightblue'"> Background Color Changed</p>
-        <p v-highlight:background.delayed="'lightblue'"> Highlight Directive</p>
-    </div>
+  <div class="directive">
+    <h1>Build-in Directives</h1>
+    <p v-text="'Some Text'"></p>
+    <p v-html="'<b><i> Some Text  </i></b>'"></p>
+    <hr />
+    <h1>Custom Directive</h1>
+    <p v-highlight="'red'">Color Changed</p>
+    <p v-highlight:background="'lightblue'">Background Color Changed</p>
+    <p v-highlight:background.delayed="'lightblue'">Highlight Directive</p>
+    <hr />
+    <h1>Local Directive</h1>
+    <p v-local-highlight:background.delayed="'yellow'">Highlight Directive</p>
+    <p v-local-highlight:background.delayed.blink="'yellow'">Blink</p>
+    <p v-local-highlight:background.blink="'yellow'">Just Blink</p>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Directives"
-}
+  name: "Directives",
+  directives: {
+    "local-highlight": {
+      bind(el, binding, vnode) {
+        let delay = 0;
+        if (binding.modifiers["delayed"]) {
+          delay = 3000;
+        }
+        if (binding.modifiers["blink"]) {
+          let mainColor = binding.value;
+          let secondColor = "coral";
+          let currentColor = mainColor;
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor =
+                currentColor === mainColor ? secondColor : mainColor;
+              if (binding.arg === "background") {
+                el.style.backgroundColor = currentColor;
+              } else {
+                el.style.color = currentColor;
+              }
+            }, 1000);
+          }, delay);
+        } else {
+          setTimeout(() => {
+            if (binding.arg === "background") {
+              el.style.backgroundColor = binding.value;
+            } else {
+              el.style.color = binding.value;
+            }
+          }, delay);
+        }
+
+        console.log(binding.arg, vnode);
+      }
+    }
+  }
+};
 </script>
 
 <style lang="css" scoped>
-    .directive{
-        width: 60%;
-        margin: 25px auto;
-    }
+.directive {
+  width: 60%;
+  margin: 25px auto;
+}
 </style>
