8ac4f54dcb7707877f2511074f439c6a8e204c21
Mehran <imganjgahi@gmail.com>
2020-07-08 15:22:56 +0430

    link and push to route

--- apps/cli/project/src/App.vue
+++ apps/cli/project/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-   <h1>App Layout</h1>
-   <hr>
+   <nav>
+     <router-link to="/" class="app-link">Home</router-link>
+     <router-link to="/user" class="app-link">User Page</router-link>
+   </nav>
    <router-view></router-view>
   </div>
       
@@ -16,3 +18,25 @@ export default {
   })
 };
 </script>
+
+<style>
+
+nav {
+  background-color: #444;
+  height: 40px;
+  display: flex;
+  align-items: center;
+}
+.app-link {
+  color: white;
+  display: inline-block;
+  margin: 0 10px;
+  text-decoration: none;
+	padding: 5px;
+}
+.router-link-exact-active {
+	background-color: white;
+	color: black;
+	border-radius: 5px;
+}
+</style>
--- apps/cli/project/src/components/Pages/User.vue
+++ apps/cli/project/src/components/Pages/User.vue
@@ -1,5 +1,17 @@
 <template>
     <div class="userPage">
         <h1>User Page</h1>
+        <div class="action">
+            <button @click="navigateToHome">Go Home</button>
+        </div>
     </div>
 </template>
+<script>
+export default {
+    methods: {
+        navigateToHome() {
+            this.$router.push("/")
+        }
+    }
+}
+</script>
