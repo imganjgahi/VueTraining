f35be06cd1ec7d24cc03e9983059eca2d9234b80
Mehran <imganjgahi@gmail.com>
2020-07-18 19:15:17 +0430

    VUEX actions

--- apps/cli/project/src/components/VuexApp/Counter/ActionBtn.vue
+++ apps/cli/project/src/components/VuexApp/Counter/ActionBtn.vue
@@ -1,18 +1,23 @@
 <template>
     <div class="actionbtn">
-        <button @click="increment">UP</button>
-        <button @click="decrement">Down</button>
+        <button @click="asyncIncrement(300)">async UP</button>
+        <button @click="decrement(2)">Down</button>
+        <button @click="increment(2)">UP</button>
     </div>
 </template>
 
 <script>
-import { mapMutations } from 'vuex'
+import { mapActions } from 'vuex'
 export default {
     methods: {
-        ...mapMutations([
+        ...mapActions([
             "increment",
             "decrement",
+            // "asyncIncrement"
         ]),
+        asyncIncrement(by) {
+            this.$store.dispatch("asyncIncrement", {by, duration: 3000})
+        }
         //before MapMutations
         // countEvent(amount){
         //     if(amount > 0){
--- apps/cli/project/src/store/store.js
+++ apps/cli/project/src/store/store.js
@@ -5,22 +5,37 @@ Vue.use(Vuex)
 
 export const store = new Vuex.Store({
     state: {
-        counter: 0
+        counter: 0,
     },
     getters: {
         doubleCounter:  state => {
             return state.counter * 2;
         },
         stringCounter:  state => {
-            return state.counter + " Times!";
+            return state.counter;
         }
     },
     mutations: {
-        increment: state => {
-            state.counter++;
+        increment: (state, payload = 1) => {
+            state.counter += payload;
         },
-        decrement: state => {
-            state.counter--;
+        decrement: (state, payload) => {
+            state.counter -= payload;
+        },
+    },
+    actions: {
+        asyncIncrement: ({ commit }, payload) => {
+            // clearTimeout()
+            setTimeout(() => {
+                console.log("setTimeout")
+                commit('increment', payload.by)
+            }, payload.duration);
+        },
+        increment: ({ commit }, payload) => {
+                commit('increment', payload)
+        },
+        decrement: ({ commit }, payload) => {
+                commit('decrement', payload)
         },
     }
 })
